24bb1f4d1e3960284725614576d067cf688eb45f
Jake Levy <levyj63@rowan.edu>
2020-09-17 20:46:41 +0000

    Added some code to pipe.js

--- streams/pipe.js
+++ streams/pipe.js
@@ -3,4 +3,14 @@ var myRead = require('./custReadStream.js');
 
 let writer = new myWrite();
 let myReader = new myRead();
+
+myReader.name = "Default Reader";
+writer.on('pipe', (src) => {
+
+    console.log(`${src.name} is piping into a Writable Stream`);
+});
+myReader.on('end', () =>{
+    console.log("Stream has ended");
+});
+
 myReader.pipe(writer);
